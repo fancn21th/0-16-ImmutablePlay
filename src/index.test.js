a743128b6034b14cc6756539c4693b20c038ea73
fancn <fanyj@yikeer.com>
2017-05-06 08:14:59 +0800

    first immutable testing added

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,19 @@
-import foo from './index'
+import Immutable from 'immutable'
 
-describe('first test', function () {
-  it('foo equal to bar', function () {
-    expect(foo()).toBe('bar')
+describe('Immutable Map Sample Test', function () {
+  it('should create Map with matching keys', function () {
+    const data = {
+      "TODO1" : {
+        title: 'TODO 1',
+        value: 'Make it happen'
+      },
+      "TODO2" : {
+        title: 'TODO 2',
+        value: 'Make it happen'
+      },
+    }
+
+    let map = Immutable.Map(data)
+    expect(map.get("TODO1").title).toEqual("TODO 1")
   })
 })
