4f1a76046ae9704d518f93435f35df5bfaa02f6a
fancn <fanyj@yikeer.com>
2017-05-06 09:30:32 +0800

    simple map test cases 1. common data 2. common data from array tuples 3. common data from general data creation function

--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,17 @@
 import Immutable from 'immutable'
+import _ from 'lodash'
+
+const createObjectTodos = numTodos => {
+  let obj = {}
+  _.range(numTodos).forEach( index => {
+    const todoSequence = String(index + 1)
+    obj['TODO' + todoSequence] = {
+      title: 'Todo ' + todoSequence,
+      value: `Make ${todoSequence} happen`
+    }
+  })
+  return obj
+}
 
 describe('Immutable Map Sample Test', function () {
   it('should create Map with matching keys', function () {
@@ -15,5 +28,15 @@ describe('Immutable Map Sample Test', function () {
 
     let map = Immutable.Map(data)
     expect(map.get("TODO1").title).toEqual("TODO 1")
+  })
+
+  it('should create Map with keys from array tuples', function () {
+    let map = Immutable.Map([["TODO1", {title: 'TODO 1'}]])
+    expect(map.get('TODO1').title).toEqual("TODO 1")
+  })
+
+  it('should create Map with matching size to number of keys', function () {
+    let map = Immutable.Map(createObjectTodos(3))
+    expect(map.size).toEqual(3)
   })
 })
