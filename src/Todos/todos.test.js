9cc38f0c71447337ee3950b0ccd5330d2d25b35d
fancn <fanyj@yikeer.com>
2017-05-08 21:10:14 +0800

    add eslint against test files

--- src/Todos/todos.test.js
+++ src/Todos/todos.test.js
@@ -2,7 +2,7 @@ import Immutable from 'immutable'
 import Todo from '../Todo'
 import * as todosActions from './index'
 
-describe('Modifying an Immutable Map()', function () {
+describe('Modifying an Immutable Map()', () => {
   it('should add todo to state', () => {
     const todo = new Todo('Todo 1', 'I am a todo', false)
     let todos = Immutable.Map()
--- src/index.test.js
+++ src/index.test.js
@@ -1,42 +1,42 @@
 import Immutable from 'immutable'
 import _ from 'lodash'
 
-const createObjectTodos = numTodos => {
-  let obj = {}
-  _.range(numTodos).forEach( index => {
+const createObjectTodos = (numTodos) => {
+  const obj = {}
+  _.range(numTodos).forEach((index) => {
     const todoSequence = String(index + 1)
-    obj['TODO' + todoSequence] = {
-      title: 'Todo ' + todoSequence,
-      value: `Make ${todoSequence} happen`
+    obj[`TODO${todoSequence}`] = {
+      title: `TODO ${todoSequence}`,
+      value: `Make ${todoSequence} happen`,
     }
   })
   return obj
 }
 
-describe('Immutable Map Sample Test', function () {
-  it('should create Map with matching keys', function () {
+describe('Immutable Map Sample Test', () => {
+  it('should create Map with matching keys', () => {
     const data = {
-      "TODO1" : {
+      TODO1: {
         title: 'TODO 1',
-        value: 'Make it happen'
+        value: 'Make it happen',
       },
-      "TODO2" : {
+      TODO2: {
         title: 'TODO 2',
-        value: 'Make it happen'
+        value: 'Make it happen',
       },
     }
 
-    let map = Immutable.Map(data)
-    expect(map.get("TODO1").title).toEqual("TODO 1")
+    const map = Immutable.Map(data)
+    expect(map.get('TODO1').title).toEqual('TODO 1')
   })
 
-  it('should create Map with keys from array tuples', function () {
-    let map = Immutable.Map([["TODO1", {title: 'TODO 1'}]])
-    expect(map.get('TODO1').title).toEqual("TODO 1")
+  it('should create Map with keys from array tuples', () => {
+    const map = Immutable.Map([['TODO1', { title: 'TODO 1' }]])
+    expect(map.get('TODO1').title).toEqual('TODO 1')
   })
 
-  it('should create Map with matching size to number of keys', function () {
-    let map = Immutable.Map(createObjectTodos(3))
+  it('should create Map with matching size to number of keys', () => {
+    const map = Immutable.Map(createObjectTodos(3))
     expect(map.size).toEqual(3)
   })
 })
