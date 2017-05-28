6872880368a9b1926f35a97546ac84f0fbae6e63
alex.fan <alex.fan@okchem.com>
2017-05-28 10:03:05 +0800

    test against api

--- src/Todos/index.js
+++ src/Todos/index.js
@@ -2,7 +2,7 @@ export const addTodo = (todos, todo) => todos.set(todo.id, todo)
 
 export const removeTodo = (todos, todo) => todos.delete(todo.id, todo)
 
-export const updateTodo = (todos, todo) => todo.update(todo.id, todo)
+export const updateTodo = (todos, todo) => todos.update(todo.id, todoS => todoS)
 
 export const mergeTodos = (todos, todos2) => todos.merge(todos2)
 
--- src/Todos/todos.test.js
+++ src/Todos/todos.test.js
@@ -1,4 +1,5 @@
 import Immutable from 'immutable'
+import _ from 'lodash'
 import Todo from '../Todo'
 import * as todosActions from './index'
 
@@ -8,5 +9,33 @@ describe('Modifying an Immutable Map()', () => {
     let todos = Immutable.Map()
     todos = todosActions.addTodo(todos, todo)
     expect(todos.get(todo.id)).toEqual(todo)
+  })
+  it('should remove todo from state', () => {
+    const todo = new Todo('Todo 1', 'I am a todo', false)
+    let todos = Immutable.Map()
+    todos = todosActions.addTodo(todos, todo)
+    todos = todosActions.removeTodo(todos, todo)
+    expect(todos.size).toEqual(0)
+  })
+  it('should update todo', () => {
+    const todo = new Todo('Todo 1', 'I am a todo', false)
+    let todos = Immutable.Map()
+    todos = todosActions.addTodo(todos, todo)
+    todo.title = 'Todo 11'
+    todos = todosActions.updateTodo(todos, todo)
+    expect(todos.get(todo.id).title).toEqual('Todo 11')
+  })
+  it('should remove all todos', () => {
+    let todos = Immutable.Map()
+    _.range(10).forEach((index) => {
+      todos = todosActions.addTodo(todos, new Todo(
+        `Todo ${index}`,
+        'I am a todo',
+        false,
+      ))
+    })
+    expect(todos.size).toEqual(10)
+    todos = todosActions.clearAll(todos)
+    expect(todos.size).toEqual(0)
   })
 })
